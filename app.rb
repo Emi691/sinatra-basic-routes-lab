30ba0ca7c0b01d862dffc3320795ba2f948c5dba
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-19 12:55:23 -0800

    commit tree fe23099a40907266bba7b300b588c803ea419cf6

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get '/name' do
+    "My name is Emiko"
+end
+
+get '/hometown' do
+    "My hometown is Berkeley"
+end
+
+get '/favorite-song' do
+    "My favorite song is Coffee in Bed"
+end
+
 end
